fc5eaf44dc40b6eeaea231f62fb319a2112f827a
Shobhakumari Singh <shobhasingh50915@gmail.com>
2026-08-04 20:57:44 +0530

    Add average.c and pointer_basic.c

---
+++ Program-PPS/pointer_basic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+int age=25;
+int *ptr;
+ptr= &age;
+
+printf("The age is %d",age);
+printf("\nThe address of age is %p",(void*)&age);
+
+printf("\n\n-------In Ptr-------");
+printf("\n\nThe age is %d",*ptr);
+printf("\nThe address of age is %p",(void*)ptr);
+
+printf("\n\n-------New value in Ptr-------");
+*ptr=30;
+printf("\nThe age is %d",*ptr);
+
+return 0;
+}
