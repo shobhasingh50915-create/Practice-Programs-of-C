d7f12b60948902605d30bd7d34ae0121a2749066
shobhasingh50915-create <shobhasingh50915@gmail.com>
2026-08-21 23:57:16 +0530

    Update power.c

--- Program-C/power.c
+++ Program-C/power.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
 int main(){
-int base,exp;
+double base;
+int exp;
 long double result=1.0;
 
 printf("Enter a base no:-");
-scanf("%d",&base);
+scanf("%lf",&base);
 
-printf("Enter an exponential-");
-scanf("%d",&exp);
+printf("Enter an exponential:-");
+scanf("%d", &exp);
 
 int tempExp=exp;
 if(exp<0){
     base=1.0/base;
-    exp=-exp;
+    exp= -exp;
 }
 while(exp>0){
-    result* = base;
+    result *= base;
     --exp;
 }
 
-printf("Answer=%Lf\n",result);
+printf("Answer= %Lf\n",result);
 
 
     return 0;
