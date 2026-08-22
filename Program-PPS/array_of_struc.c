e06ed149fb257e8e98e86af90677723f11c1c0ef
shobhasingh50915-create <shobhasingh50915@gmail.com>
2026-08-23 00:03:16 +0530

    Update array_of_struc.c

--- Program-PPS/array_of_struc.c
+++ Program-PPS/array_of_struc.c
@@ -8,7 +8,7 @@ struct Employee{
 int main(){
     struct Employee emp[3];
     int i;
-    for(i=0;i<n;i++){
+    for(i=0;i<3;i++){
        printf("Enter the details of Employee:");
        printf("Enter id:");
        scanf("%d\n",&emp[i].id);
@@ -16,6 +16,13 @@ int main(){
        scanf("%s\n",&emp[i].name);
        printf("Enter salary:");
        scanf("%f\n",&emp[i].salary);
-
     }
-}
+      while(i<3) {
+       printf("The details of Employee:");
+       printf("Id is %d",&emp[i].id);
+       printf("Name is %s",&emp[i].name);
+       printf("Salary is %f",&emp[i].salary);
+       i++;
+    }
+      return 0;
+    }
