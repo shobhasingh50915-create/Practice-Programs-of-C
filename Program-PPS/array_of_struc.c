cad082f3693df15f84d2c77d777981562fbcfcb9
shobhasingh50915-create <shobhasingh50915@gmail.com>
2026-08-23 00:31:29 +0530

    Update array_of_struc.c

--- Program-PPS/array_of_struc.c
+++ Program-PPS/array_of_struc.c
@@ -9,20 +9,21 @@ int main(){
     struct Employee emp[3];
     int i;
     for(i=0;i<3;i++){
-       printf("Enter the details of Employee:");
-       printf("Enter id:");
-       scanf("%d\n",&emp[i].id);
+       printf("Enter the details of Employee[%d]:-",i);
+       printf("\nEnter id:");
+       scanf("%d",&emp[i].id);
        printf("Enter name:");
-       scanf("%s\n",&emp[i].name);
+       scanf("%s",&emp[i].name);
        printf("Enter salary:");
-       scanf("%f\n",&emp[i].salary);
+       scanf("%f",&emp[i].salary);
     }
-      while(i<3) {
-       printf("The details of Employee:");
-       printf("Id is %d",&emp[i].id);
-       printf("Name is %s",&emp[i].name);
-       printf("Salary is %f",&emp[i].salary);
-       i++;
+      
+      for(i=0;i<3;i++){
+       printf("\n\n------The records of Employee [%d]------",i);
+       printf("\nId is %d",emp[i].id);
+       printf("\nName is %s",emp[i].name);
+       printf("\nSalary is %f",emp[i].salary);
+       
     }
       return 0;
     }
